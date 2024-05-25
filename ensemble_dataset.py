b4d5e77e6238d2c77c6d6fe3132acc57fcec6160
niryarden <niryar@gmail.com>
2024-05-25 15:38:04 +0300

    finished multithreading

--- ensemble_dataset.py
+++ ensemble_dataset.py
@@ -2,9 +2,15 @@ import json
 import urllib.request
 from bs4 import BeautifulSoup
 import threading
+import os
 
 
 REQ_RETRIES = 3
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def get_response(game_id, year):
@@ -71,47 +77,56 @@ def parse_metadata(data):
     return metadata
 
 
-def scrape_game(game_id, year):
+def scrape_game(game_id, year, get_recaps):
     response, is_success = get_response(game_id, year)
     if not is_success:
         return
     soup = BeautifulSoup(response, "html.parser")
     data = json.loads(soup.find('script', type='application/json', id="__NEXT_DATA__").text)
-    recap_as_list = data["props"]["pageProps"]["story"]["content"]
-    recap = parse_recap(recap_as_list)
     actions = data["props"]["pageProps"]["playByPlay"]["actions"]
     play_by_play = parse_play_by_play(actions)
     metadata = parse_metadata(data)
     game = {
         "metadata": metadata,
-        "input": play_by_play,
-        "output": recap
+        "input": play_by_play
     }
+    if get_recaps:
+        recap_as_list = data["props"]["pageProps"]["story"]["content"]
+        recap = parse_recap(recap_as_list)
+        game["output"] = recap
     with open(f"data/dataset/{year}/{year}_samples.jsonl", "a") as f:
         f.write(json.dumps(game) + "\n")
         f.flush()
 
 
-def scrape_unsuccessful_games(year):
+def scrape_unsuccessful_games(year, get_recaps):
     with open(f"data/dataset/{year}/unsuccessful_game_ids.txt", 'r') as f:
         game_ids = f.read().splitlines()
     with open(f"data/dataset/{year}/unsuccessful_game_ids.txt", 'w') as f:
         pass
     for game_id in game_ids:
-        scrape_game(game_id, year)
+        scrape_game(game_id, year, get_recaps)
 
 
-def scrape_year(year):
+def scrape_year(year, get_recaps):
+    create_dir(f"data/dataset/{year}")
     with open(f"data/game_ids/{year}.txt") as f:
         game_ids = f.read().splitlines()
     for game_id in game_ids:
-        scrape_game(game_id, year)
+        scrape_game(game_id, year, get_recaps)
+
+    should_stop = False
+    while not should_stop:
+        scrape_unsuccessful_games(year, get_recaps)
+        with open(f"data/dataset/{year}/unsuccessful_game_ids.txt", 'r') as f:
+            if len(f.read()) == 0:
+                should_stop = True
 
 
 def main():
     threads = []
     for year in range(2020, 2024):
-        thread = threading.Thread(target=scrape_year, args=(year,))
+        thread = threading.Thread(target=scrape_year, args=(year, True,))
         threads.append(thread)
         thread.start()
     for thread in threads:
